9c1c0e15a4fcb783dff790ddefd93f758a390ef5
PaulRMIT <s3755840@student.rmit.edu.au>
2023-03-30 20:33:27 +1100

    modified:   src/Webhook.ino

--- src/Webhook.ino
+++ src/Webhook.ino
@@ -1,17 +1,13 @@
 // This #include statement was automatically added by the Particle IDE.
 #include <Adafruit_DHT.h>
 
-#define DHTPIN 2
+#define DHTPIN D2
 
 #define DHTTYPE DHT22
 
 DHT dht(DHTPIN, DHTTYPE);
 
-int led = D7;
-
 void setup() {
-    
-    pinMode(led, OUTPUT);
     
     dht.begin();
 
@@ -21,16 +17,9 @@ void loop() {
     
     digitalWrite(led, HIGH);
     
-    float h = dht.getHumidity();
-    
     float t = dht.getTempCelcius();
     
-    String temp = String(t);
     Particle.publish("Temperature", String(t), PRIVATE);
-    Particle.publish("Humidity", String(h), PRIVATE);
-	
-    delay(30000);
-    
-    digitalWrite(led, LOW);
+
     delay(30000);
 }
